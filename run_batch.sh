709a69a2a0862aae768ae297ced2e3833795a4ca
Hwiwon Lee <hwiwonl2@illinois.edu>
2025-03-17 18:08:27 -0500

    Adjust batch script

--- run_batch.sh
+++ run_batch.sh
@@ -15,13 +15,13 @@ if [ "$mode" != "infer" ] && [ "$mode" != "eval" ]; then
 fi
 
 if [ "$mode" == "infer" ]; then
-    ./run_infer.sh -m o3 -c 1.0 -t 0.0 -s test -l :20 -n 75 -b "https://litellm-proxy-153298433405.us-east1.run.app/"
     ./run_infer.sh -m 4o -c 1.0 -t 0.0 -s test -l :20 -n 75 -b "https://litellm-proxy-153298433405.us-east1.run.app/"
+    ./run_infer.sh -m o3 -c 1.0 -t 0.0 -s test -l :20 -n 75 -b "https://litellm-proxy-153298433405.us-east1.run.app/"
     ./run_infer.sh -m sonnet -c 1.5 -t 0.0 -s test -l :20 -n 75 -b "https://litellm-proxy-153298433405.us-east1.run.app/"
 elif [ "$mode" == "eval" ]; then
     # Get the user ID
     id=$(whoami)
-    python run_eval.py --input-file trajectories/${id}/secbench__claude-3-5-sonnet-20241022__t-0.00__p-0.95__c-1.50___secbench_test/preds.json
     python run_eval.py --input-file trajectories/${id}/secbench__gpt-4o__t-0.00__p-0.95__c-1.00___secbench_test/preds.json
     python run_eval.py --input-file trajectories/${id}/secbench__o3-mini__t-0.00__p-0.95__c-1.00___secbench_test/preds.json
+    python run_eval.py --input-file trajectories/${id}/secbench__claude-3-5-sonnet-20241022__t-0.00__p-0.95__c-1.50___secbench_test/preds.json
 fi
